79c5f9199ebd26a3c56d0193f0abd8c596dc1759
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2022-10-15 11:29:21 -0400

    Added taskGUI and tkinter as our GUI

---
+++ taskGUI.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+window = tk.Tk()
+
+greeting = tk.Label(text = "Hello, Tkinter")
+greeting.pack()
+
+window.mainloop()
+# tk = Tk()
+# frame = Frame(tk, borderwidth=2)
+# frame.pack(fill=BOTH, expand=1)
+# label = Label(frame, text="Button Example")
+# label.pack(fill=X, expand=1)
+#
+# button = Button(frame, text="Exit", command=tk.destroy)
+# button.pack(side=BOTTOM)
+# tk.mainloop()
