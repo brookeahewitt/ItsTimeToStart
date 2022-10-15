4bb4400bf98838f2e68b639a996458decb21f02f
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2022-10-15 13:16:22 -0400

    Added start and stop timer buttons

--- taskGUI.py
+++ taskGUI.py
@@ -2,10 +2,39 @@ import tkinter as tk
 #Tkinter  tutorial link: https://realpython.com/python-gui-tkinter/
 window = tk.Tk()
 
+window.title("Timer") #Temporary Title
+
 greeting = tk.Label(text = "Hello, Tkinter")
 greeting.pack()
 
+label = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black
+)
+label.pack()
+
+startButton = tk.Button(
+    text="Start Timer",
+    width=10,
+    height=2,
+    bg="green",
+    fg="white",
+)
+startButton.pack()
+
+stopButton = tk.Button(
+    text="Stop Timer",
+    width=10,
+    height=2,
+    bg="red",
+    fg="white",
+)
+stopButton.pack()
+
+
 window.mainloop()
+
 # tk = Tk()
 # frame = Frame(tk, borderwidth=2)
 # frame.pack(fill=BOTH, expand=1)
