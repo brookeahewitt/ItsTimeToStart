4ccf0e0c1cc49c7ebcf5d3c3069ef5b76c83aded
Brooke Hewitt <dnj6xk@virginia.edu>
2022-10-15 10:56:13 -0400

    Created taskName class

---
+++ taskName.py
@@ -0,0 +1,16 @@
+class Task:
+    startTime = 0
+    endTime = 0
+    goalTime = 0
+    taskName = ""
+
+    def __init__(self, tn, st, gt):
+        self.taskName = tn
+        self.startTime = st
+        self.goalTime = gt
+
+    def setEndTime(self, et):
+        self.endTime = et
+
+    def getElapsedTime(self):
+        return self.endTime - self.startTime
